f5b2ebd7393f5b0e16ba8b46452d730fd8343fc7
League Student <league@iMac-27.attlocal.net>
2020-02-28 16:55:53 -0800

    Worked on Hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -13,10 +13,14 @@ public class HangMan implements KeyListener {
 	JFrame f;
 	JPanel p;
 	JLabel l;
+	JLabel l2;
 	int number = new Integer(0);
 	Stack<String> words = new Stack<String>();
 	String currentWord = new String();
 	int wordSize = new Integer(0);
+	Boolean won = false;
+	Boolean replacedThing = false;
+	int lives=new Integer(10);
 
 	public static void main(String[] args) {
 		HangMan hangman = new HangMan();
@@ -30,8 +34,10 @@ public class HangMan implements KeyListener {
 		f = new JFrame();
 		p = new JPanel();
 		l = new JLabel();
+		l2 = new JLabel();
 		f.add(p);
 		p.add(l);
+		p.add(l2);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setVisible(true);
 		f.addKeyListener(this);
@@ -50,35 +56,73 @@ public class HangMan implements KeyListener {
 	}
 
 	public void makeLabel() {
-		String blank = " ";
+		String blank = "";
 		for (int i = 0; i < wordSize; i++) {
 			blank += "_ ";
 		}
 		l.setText(blank);
+		l2.setText("You have " + lives + " lives");
 		f.pack();
+	}
+
+	public void checkIfWon() {
+		if (!l.getText().contains("_")) {
+			won = true;
+			words.pop();
+			startCode();
+		}
+	}
+	
+	public void startCode() {
+		lives =10;
+		l.setText(" ");
+		f.pack();
+		makeFrame();
+		getWords();
+		makeLabel();
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		replacedThing = false;
 		for (int i = 0; i < currentWord.length(); i++) {
-			if (e.getKeyChar()== currentWord.charAt(i)) {
+			if (e.getKeyChar() == currentWord.charAt(i)) {
 				String labeltext = l.getText();
-				String replaced = labeltext.substring(0,(i*2))+currentWord.charAt(i)+labeltext.subSequence((i*2)+2, labeltext.length());
+				String replaced = labeltext.substring(0, (i * 2)) + currentWord.charAt(i)
+						+ labeltext.substring((i * 2) + 1, labeltext.length());
 				l.setText(replaced);
+				replacedThing = true;
+				f.pack();
+				checkIfWon();
+			}
 		}
-}	
+		if (replacedThing == false) {
+			String yesorno = new String();
+			lives = lives - 1;
+			l2.setText("You have " + lives + " lives");
+			if (lives == 0) {
+				yesorno = JOptionPane.showInputDialog("You failed, Do you want to play again?");
+				if (yesorno.equalsIgnoreCase("yes")) {
+					startCode();
+				}
+				else {
+					System.exit(0);
+				}
+			}
+
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
